d7fdd054baa107a63b33ee7384d75e175a9f57e6
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2021-11-11 20:53:41 +0800

    Remove function related to okhttp, okgo and umeng

--- app/src/main/java/com/surcumference/fingerprint/network/updateCheck/UpdateFactory.java
+++ app/src/main/java/com/surcumference/fingerprint/network/updateCheck/UpdateFactory.java
@@ -2,7 +2,6 @@ package com.surcumference.fingerprint.network.updateCheck;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.Nullable;
@@ -22,23 +21,12 @@ import com.surcumference.fingerprint.util.ApplicationUtils;
 import com.surcumference.fingerprint.util.Config;
 import com.surcumference.fingerprint.util.FileUtils;
 import com.surcumference.fingerprint.util.Task;
-import com.surcumference.fingerprint.util.ZipUtils;
 import com.surcumference.fingerprint.util.log.L;
-import com.surcumference.fingerprint.view.DownloadView;
-import com.surcumference.fingerprint.view.MagiskInstPluginTargetSelectionView;
-import com.surcumference.fingerprint.view.MessageView;
-import com.surcumference.fingerprint.view.ShellExecuteView;
-import com.surcumference.fingerprint.view.UpdateInfoView;
-
-import net.lingala.zip4j.exception.ZipException;
 
 import java.io.File;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.Callable;
-
-import eu.chainfire.libsuperuser.Shell;
 
 /**
  * Created by Jason on 2017/9/10.
@@ -79,118 +67,13 @@ public class UpdateFactory {
 
                 @Override
                 public void onHasUpdate(UpdateInfo updateInfo) {
-                    if (!dontSkip) {
-                        if (isSkipVersion(context, updateInfo.version)) {
-                            L.d("已跳過版本: " + updateInfo.version);
-                            return;
-                        }
-                    }
-                    UpdateInfoView updateInfoView = new UpdateInfoView(context);
-                    updateInfoView.setTitle(Lang.getString(R.id.found_new_version) + updateInfo.version);
-                    updateInfoView.setContent(updateInfo.content);
-                    updateInfoView.withOnNeutralButtonClickListener((dialogInterface, i) -> {
-                        Config.from(context).setSkipVersion(updateInfo.version);
-                        dialogInterface.dismiss();
-                    });
-                    updateInfoView.withOnPositiveButtonClickListener((dialogInterface, i) -> {
-                        PluginApp.runActionBaseOnCurrentPluginType(new HashMap<PluginType, Callable<Object>>() {{
-                            put(PluginType.Magisk, () -> {
-                                handleMagiskUpdate(context, updateInfo, dialogInterface);
-                                return null;
-                            });
-                            put(PluginType.Xposed, () -> {
-                                handleXposedUpdate(context, updateInfo, dialogInterface);
-                                return null;
-                            });
-                        }});
-                    });
-                    updateInfoView.showInDialog();
+                    // Removed
                 }
             }).doUpdateCheck();
         } catch (Exception | Error e) {
             //for OPPO R11 Plus 6.0 NoSuchFieldError: No instance field mResultListener
             L.e(e);
         }
-    }
-
-    private static void handleMagiskUpdate(Context context, UpdateInfo updateInfo, DialogInterface updateInfoViewDialogInterface) {
-        Task.onBackground(() -> {
-            if (!Shell.SU.available()) {
-                Task.onMain(() -> new MessageView(context).text(Lang.getString(R.id.update_no_root)).showInDialog());
-                return;
-            }
-            Task.onMain(() -> {
-                MagiskInstPluginTargetSelectionView instPluginTargetSelectionView = new MagiskInstPluginTargetSelectionView(context);
-                instPluginTargetSelectionView.showInDialog();
-                instPluginTargetSelectionView.withOnPositiveButtonClickListener((dialog, which) -> {
-                    Map<PluginTarget, Boolean> instPluginTargetSelectionMap = instPluginTargetSelectionView.getSelection();
-                    L.d("instPluginTargetSelectionMap", instPluginTargetSelectionMap);
-                    if (!instPluginTargetSelectionMap.values().contains(true)) {
-                        Toast.makeText(context, Lang.getString(R.id.update_at_least_select_one), Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                    dialog.dismiss();
-                    String fileName = context.getPackageName() + ".zip";
-                    File cacheDir = context.getCacheDir();
-                    File targetFile = new File(cacheDir, fileName);
-                    File unzipDir = new File(cacheDir, BuildConfig.APPLICATION_ID);
-                    Runnable cleanTask = () -> {
-                        FileUtils.delete(targetFile);
-                        FileUtils.delete(unzipDir);
-                    };
-                    cleanTask.run();
-                    new DownloadView(context)
-                            .download(updateInfo.url, targetFile, () -> {
-                                    updateInfoViewDialogInterface.dismiss();
-                                    ShellExecuteView shellExecuteView = new ShellExecuteView(context);
-                                    shellExecuteView.showInDialog();
-                                    Task.onBackground(() -> {
-                                        try {
-                                            unzipDir.mkdirs();
-                                            try {
-                                                ZipUtils.unzip(targetFile, unzipDir.getAbsolutePath(), "");
-                                            } catch (ZipException e) {
-                                                L.e(e);
-                                                Task.onMain(() -> new MessageView(context).text(Lang.getString(R.id.update_file_corrupted)).showInDialog());
-                                                return;
-                                            }
-
-                                            File[] moduleZipFiles = unzipDir.listFiles();
-                                            Map<PluginTarget, File> moduleFilePluginTargetMap = matchMagiskModuleFileListToPluginTarget(moduleZipFiles);
-                                            L.d("moduleFilePluginTargetMap", moduleFilePluginTargetMap);
-                                            if (moduleFilePluginTargetMap.size() <= 0) {
-                                                Task.onMain(() -> new MessageView(context).text(Lang.getString(R.id.update_file_missing)).showInDialog());
-                                                return;
-                                            }
-                                            Iterator<Map.Entry<PluginTarget, Boolean>> it = instPluginTargetSelectionMap.entrySet().iterator();
-                                            while (it.hasNext()) {
-                                                Map.Entry<PluginTarget, Boolean> entry = it.next();
-                                                PluginTarget pluginTarget = entry.getKey();
-                                                boolean selected = (boolean)entry.getValue();
-                                                if (!selected) {
-                                                    continue;
-                                                }
-                                                File moduleFile = moduleFilePluginTargetMap.get(pluginTarget);
-                                                if (moduleFile == null) {
-                                                    continue;
-                                                }
-                                                String command = String.format("magisk --install-module \"%s\"", moduleFile.getAbsolutePath());
-                                                int installModuleResult = shellExecuteView.executeCommand(command);
-                                                if (installModuleResult != 0) {
-                                                    Task.onMain(()-> shellExecuteView.appendCommandLineOutput(Lang.getString(R.id.update_installation_failed) + installModuleResult + "\n请尝试前往更新页面手动获取更新"));
-                                                    return;
-                                                }
-                                            }
-                                            Task.onMain(()-> shellExecuteView.appendCommandLineOutput(Lang.getString(R.id.update_success_note)));
-                                        } finally {
-                                            cleanTask.run();
-                                        }
-                                    });
-                            }).showInDialog();
-
-                });
-            });
-        });
     }
 
     private static Map<PluginTarget, File> matchMagiskModuleFileListToPluginTarget(@Nullable File[] moduleZipFiles) {
@@ -207,18 +90,6 @@ public class UpdateFactory {
             }
         });
         return map;
-    }
-
-    private static void handleXposedUpdate(Context context, UpdateInfo updateInfo, DialogInterface updateInfoViewDialogInterface) {
-        String fileName = context.getPackageName() + ".apk";
-        File targetFile = FileUtils.getSharableFile(context, fileName);
-        FileUtils.delete(targetFile);
-        new DownloadView(context)
-                .download(updateInfo.url, targetFile, () -> {
-                    updateInfoViewDialogInterface.dismiss();
-                    UpdateFactory.installApk(context, targetFile);
-                    new MessageView(context).text(Lang.getString(R.id.update_success_note)).showInDialog();
-                }).showInDialog();
     }
 
     public static void lazyUpdateWhenActivityAlive() {
--- app/src/main/java/com/surcumference/fingerprint/network/updateCheck/github/GithubUpdateChecker.java
+++ app/src/main/java/com/surcumference/fingerprint/network/updateCheck/github/GithubUpdateChecker.java
@@ -19,86 +19,18 @@ import com.surcumference.fingerprint.util.log.L;
 import java.io.IOException;
 import java.util.Date;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 /**
  * Created by Jason on 2017/9/9.
  */
 
 public class GithubUpdateChecker extends BaseUpdateChecker {
-
-    public static OkHttpClient sHttpClient = new OkHttpClient();
-
     public GithubUpdateChecker(UpdateResultListener listener) {
         super(listener);
     }
 
     @Override
     public void doUpdateCheck() {
-        Callback callback;
-        callback = new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                onNetErr();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response != null && response.isSuccessful()) {
-                    String replay = response.body().string();
-                    response.close();
-                    try {
-                        GithubLatestInfo info = new Gson().fromJson(replay, GithubLatestInfo.class);
-                        if (info != null) {
-                            if (info.isDataComplete()) {
-                                if (BuildConfig.DEBUG || StringUtils.isAppNewVersion(BuildConfig.VERSION_NAME, info.version)) {
-                                    L.d("info", info);
-                                    String content = appendUpdateExtInfo(info.content, info.date, info.contentUrl);
-                                    L.d("content", content);
-                                    GithubAssetsInfo assetsInfo = info.getDownloadAssetsInfo();
-                                    UpdateInfo updateInfo = new UpdateInfo(info.version, content, info.contentUrl, assetsInfo.url, assetsInfo.size);
-                                    onHasUpdate(updateInfo);
-                                } else {
-                                    onNoUpdate();
-                                }
-                                return;
-                            }
-                        }
-                    } catch (Exception e) {
-                        L.d(e);
-                    }
-                }
-                onNetErr();
-            }
-        };
-
-        Request request = new Request.Builder()
-                .url(Constant.UPDATE_URL_GITHUB)
-                .build();
-        sHttpClient.newCall(request).enqueue(callback);
+        // Removed
+        onNoUpdate();
     }
-
-    private String appendUpdateExtInfo(String content, Date date, String pageUrl) {
-        StringBuilder sb = new StringBuilder(content);
-        if (date != null) {
-            sb.append("\n");
-            if (TextUtils.isEmpty(pageUrl)) {
-                sb.append("\n");
-            } else {
-                sb.append("<a href='");
-                sb.append(pageUrl.replaceAll("http(s)*://", ""));
-                sb.append("'>");
-                sb.append(Lang.getString(R.id.goto_update_page));
-                sb.append("</a>");
-                sb.append("\n");
-            }
-            sb.append(Lang.getString(R.id.update_time)).append(": ").append(DateUtils.toString(date));
-        }
-        return sb.toString();
-    }
-
 }
